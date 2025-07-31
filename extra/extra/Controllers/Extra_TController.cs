efb0eceb1a1af33959a16f0c2900d971b843b448
DrashtiBaldha <drashtibaldha2003@gmail.com>
2025-07-31 09:11:53 +0530

    .net basic program

---
+++ extra/extra/Controllers/Extra_TController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace extra.Controllers
+{
+    public class Extra_TController : Controller
+    {
+        // GET: Extra_T
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Digit()
+        {
+            ViewBag.Value = 22;
+            ViewBag.firstvalue = ViewBag.Value / 10;
+            ViewBag.secondvalue = ViewBag.Value % 10;
+            ViewBag.multiplication = ViewBag.firstvalue * ViewBag.secondvalue;
+            return View();
+        }
+    }
+}
